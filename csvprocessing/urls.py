39cd919cf59085d8058ebddea4bd43834659d87e
alisherrusinov <alisher.nugmanoff@gmail.com>
2021-12-16 13:23:53 +0500

    added drf

--- csvprocessing/urls.py
+++ csvprocessing/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import CreateTaskView
+
 
 urlpatterns = [
-    path('',)
+    path('create_task', CreateTaskView.as_view()),
 ]
--- csvprocessing/views.py
+++ csvprocessing/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class CreateTaskView(APIView):
+    def post(self, request):
+        filename = request.data.get('filename')
+        return Response({"success": f"Task '' created successfully"})
 
